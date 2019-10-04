0f44a1dfa643326c5b99fc48ebbfb63f038ca572
Peter Risko <lokoxx@gmail.com>
2019-10-04 17:41:57 +0200

    STICKY HEADER - CSS formating added .sticky attribute, js script added

---
+++ Resources/js/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+/* Just demo - changes the background color of h2 after it is clicked
+$(document).ready(function(){
+   $('h2').click(function(){
+      $(this).css('background-color','#ff0000') 
+   });
+    
+});
+
+*/
