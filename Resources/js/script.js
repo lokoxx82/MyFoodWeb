33fb0a4b0d2b9c921d8fadf0ce4fef1f7ed1acb3
Peter Risko <lokoxx@gmail.com>
2019-10-05 11:46:28 +0200

    Scrolling to elements from buttons and links

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,6 +1,9 @@
 /* Select section in JQuery*/
 
+
+
 $(document).ready(function(){
+    /*STICKY navigation*/
    $('.js--section-features').waypoint(function(direction){
        if(direction=='down'){
            $('nav').addClass('sticky');
@@ -9,8 +12,38 @@ $(document).ready(function(){
        }
    }, {
     offset: '60px'
-   })
+   });
+    
+    /*Scroll on buttons*/
+    $('.js--scroll-to-plans').click(function(){
+       $('html,body').animate({scrollTop:$('.js--section-plans').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-start').click(function(){
+       $('html,body').animate({scrollTop:$('.js--section-features').offset().top}, 1000); 
+    });
+    
+    /*NAVIGATIN SCROLL*/
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    
+    
 });
+
+
+
 
 
 
